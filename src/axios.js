34f26a266ebf37e175f56047c14cbfa6708696a9
Winster Mano <winstermano74@gmail.com>
2025-07-16 21:44:11 +0530

    Initial commit

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-
-const  instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3"
+const instance = axios.create({
+  baseURL: "https://cors-anywhere.herokuapp.com/https://api.themoviedb.org/3"
 });
 
 export default instance;
+
+
